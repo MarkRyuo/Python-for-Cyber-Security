3639954c0f5631c316196767d6809e6bdfdec715
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-05 10:40:55 +0800

    udpate ehrE

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ name = input("Input your name: ")
 while not name :
   name = input("Input your name: ")
 
-if name == "Mark":
+if name == "Mark" and name == "Ryuo" and name == "Moda" :
   print("Your Ban, Ngunit kung ikaw ay papasa sa Character Identity Check you can come in!")
   identity_Check1 = input("Are you a good citezen (y/n): ")
   if identity_Check1 == "y" or identity_Check1 == "Y" :
@@ -18,11 +18,7 @@ if name == "Mark":
       print("You're Totally a Ban!")
       exit()
     else :
-      print(f"{name} your pass in identity check! Welcome to Arc Coffee Shop")
-
-elif name == "Ryuo" :
-  print("Your Ban, Ngunit kung ikaw ay papasa sa Character Identity Check you can come in!")
-  
+      print(f"{name} your pass in identity check! Welcome to Arc Coffee Shop")  
 else :
   print(f"Welcome to Arc Coffee shop {name}")
   
