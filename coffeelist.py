ab9e24e959ada62289b09d42332e09e3b1e8cb12
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-05 11:10:46 +0800

    updat eherE

--- coffeelist.py
+++ coffeelist.py
@@ -2,7 +2,7 @@
 # Todo This is Object
 
 # Tuple - unchangeable 
-
+# Coffee_List is for displaying only 
 Coffee_List = (
   "Americano",
   "Mocha",
@@ -13,5 +13,6 @@ Coffee_List = (
 
 americano = {
   "Coffee_name": "Americano",
-  
+  "price" : 30,
+   
 }
