3a57e523d1085b0babe23def8413ee3ee3ce0c82
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-05 10:20:02 +0800

    update here

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import coffeelist
 # Todo Create a Secure Coffee Shop 
 print(list_ban_names)
 
-
 name = input("Input your name: ")
 
 while not name :
   name = input("Input your name: ")
 
 if name == "Mark" :
-  print("Your Ban, but ")
+  print("Your Ban, Ngunit kung ikaw ay papasa sa Character Question, Makakapasok ka")
 else :
   print(f"Welcome to Arc Coffee shop {name}")
   
