d2c16e26a207437bbfbd6245661577ab01ae028e
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-05 10:57:40 +0800

    updta eherE

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ while not name :
 
 if name in list_ban_names :
   print("Your Ban, Ngunit kung ikaw ay papasa sa Character Identity Check you can come in!")
-  identity_Check1 = input("Are you a good citezen (y/n): ")
+  identity_Check1 = input("Are you a good citizen (y/n): ") # if y/Y go to next identity check if n automatically exit
   if identity_Check1 == "y" or identity_Check1 == "Y" :
     identity_Check2 = int(input("How many good way you have: "))
     if identity_Check2 <= 3 :
