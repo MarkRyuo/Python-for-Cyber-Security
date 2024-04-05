fe127888db6e08476bfde11c300c9e8c250011ab
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-05 10:31:50 +0800

    update here

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if name == "Mark":
   print("Your Ban, Ngunit kung ikaw ay papasa sa Character Identity Check you can come in!")
   identity_Check1 = input("Are you a good citezen (y/n): ")
   if identity_Check1 == "y" or identity_Check1 == "Y" :
-    
+    identity_Check2 = input("")   
 elif name == "Ryuo" :
   print("Your Ban, Ngunit kung ikaw ay papasa sa Character Identity Check you can come in!")
   
