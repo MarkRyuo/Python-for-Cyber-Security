d220de71eeb210f9ba7fd784c60c432cf0dab453
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-05 10:28:49 +0800

    update herE

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ while not name :
 
 if name == "Mark":
   print("Your Ban, Ngunit kung ikaw ay papasa sa Character Identity Check you can come in!")
-  identity_Check1 = input("")
+  identity_Check1 = input("Are you a good citezen (y/n): ")
 elif name == "Ryuo" :
   print("Your Ban, Ngunit kung ikaw ay papasa sa Character Identity Check you can come in!")
   
