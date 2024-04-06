2ab8c50abd679f44d35c8af74f4713c9138add7b
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-06 20:22:56 +0800

    udpate herE

--- play.py
+++ play.py
@@ -1,10 +1,10 @@
 # list 
 
-fruits = ["Banana", "Apple", "Pineapple"]
+# fruits = ["Banana", "Apple", "Pineapple"]
 
 # fruits.insert(3, "Barako")
-fruits.append("Barako")
-print(fruits)
+# fruits.append("Barako")
+# print(fruits)
 
 # print(help(fruits))
 
