ba2cb0c453ab9da3bb9ab4b12191978122d7f852
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-07 22:19:52 +0800

    update herE

--- play.py
+++ play.py
@@ -36,3 +36,6 @@ removeCar = input("Do you want to remove a car in the list: (y/n)")
 
 if removeCar == "y" or removeCar == "Y" :
   carRemove = input(f"What car in the list: {car_Brand} : \n")
+  if carRemove in car_Brand :
+    car_Brand.remove()
+    print(f"New Car list: {car_Brand}")
