6c621432dcb5b511706164a8ae46e5749aca5c8e
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-11 21:58:29 +0800

    dasd

---
+++ Car Rent/main.py
@@ -0,0 +1,9 @@
+# Create a question for login 
+
+# do you want to login? YES --> Continue if No --> Exit 
+
+question = input("Do you want to login? (yes/no)") 
+
+while not question :
+    question = input("Do you want to login? (yes/no)") 
+    
