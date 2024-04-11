f1cd35a23095afbe0618143854a7bd30157406ad
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-11 22:21:37 +0800

    udpate herE'

--- Car Rent/main.py
+++ Car Rent/main.py
@@ -3,11 +3,11 @@
 # do you want to login? YES --> Continue if No --> Exit 
 
 question = input("Do you want to login? (yes/no)") 
+question.lower()
 
 while not question :
     question = input("Do you want to login? (yes/no)") 
     if question == "yes" :
-        question.lower()
         # Display the answer 
         print(f"{question}")
-    elif question == "no"  
+    elif question == "no" :  
