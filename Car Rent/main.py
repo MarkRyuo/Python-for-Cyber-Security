aece87164a5b748e40e749ffa115523c91273e61
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-12 11:51:01 +0800

    updat eherE

--- Car Rent/main.py
+++ Car Rent/main.py
@@ -2,7 +2,7 @@
 
 # do you want to login? YES --> Continue if No --> Exit 
 
-# question = input("Do you want to login? (yes/no): ") # To-do This code have a issue 
+question = input("Do you want to login? (yes/no): ") # To-do This code have a issue 
 
 # while not question :
 #     question = input("Do you want to login? (yes/no): ") 
@@ -18,10 +18,24 @@
 #         print(f"Your answer wrong {question}")
 #         exit()
 
-def Question() :
-    question = input("Do you want to login? (yes/no): ")
-    if question == None :
-        question = input("Do you want to login? (yes/no): ")
+
+
+def Question(question) : 
+    while not question :
+      question = input("Do you want to login? (yes/no): ") 
+      if question == "yes" :
+        question.lower()
+         # Display the answer 
+        print(f"{question}")
+     elif question == "no" :
+        question.lower()
+        print(f"{question}")
+        exit()
+    else: 
+        print(f"Your answer wrong {question}")
+        exit()
+    
+   
 
               
 
